6df6b2088bf83f4522f83a9a15b3f38863bc5d72
Jothinayagan <v.i.jothinayagan.307@gmail.com>
2021-06-26 17:57:50 +0530

    Structure Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import SideBar from "./Components/SideBar/SideBar";
+import NavBar from "./Components/NavBar/NavBar";
+import Main from "./Components/Main";
+import Player from "./Components/Player";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>
+                <SideBar />
+            </div>
+            <div>
+              <NavBar />
+              <Main />
+              <Player />
+            </div>
+        </div>
+    );
 }
 
 export default App;
