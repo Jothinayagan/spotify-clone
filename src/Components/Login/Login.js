68758c7dab51a48c8ac7d94be9f9c611b71146e5
Jothinayagan <v.i.jothinayagan.307@gmail.com>
2022-01-07 13:16:15 +0530

    Add Auth 2

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { SPOTIFY_LOGO_URL } from "../../utils/api";
+import { LOGIN_URI, SPOTIFY_LOGO_URL } from "../../utils/api";
 import "./Login.css";
 
 function Login() {
   return (
     <div className="login">
       <img src={SPOTIFY_LOGO_URL} />
-      <a>LOGIN WITH SPOTIFY</a>
+      <a href={LOGIN_URI}>LOGIN WITH SPOTIFY</a>
     </div>
   );
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,17 @@
 export const SPOTIFY_LOGO_URL =
   "https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg";
-export const SPOTIFY_AUTH = "https://accounts.spotify.com/authorize";
+export const SPOTIFY_AUTH_URI = "https://accounts.spotify.com/authorize";
+export const SPOTIFY_CLIENT_ID = "011f9c47ceb342929fefd4cae8cc7179";
+export const REDIRECT_URI = "http://localhost:3000/";
+
+export const SPOTIFY_SCOPE = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const LOGIN_URI = `${SPOTIFY_AUTH_URI}?client_id=${SPOTIFY_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SPOTIFY_SCOPE.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
