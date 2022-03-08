0b362b49201d80b9169114c43d7fda78006fd447
Jothinayagan <v.i.jothinayagan.307@gmail.com>
2022-03-08 10:39:13 +0530

    feat: add footer & remove scroll bar

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,12 +1,52 @@
 import React from "react";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import ShuffleIcon from "@mui/icons-material/Shuffle";
+import RepeatIcon from "@mui/icons-material/Repeat";
+import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
+import VolumeDownIcon from "@mui/icons-material/VolumeDown";
+import { Grid, Slider } from "@mui/material";
 import "./style.css";
 
 function Footer() {
-  return (
-    <div className="footer">
-      <h1>I'm the Footer</h1>
-    </div>
-  );
+    return (
+        <div className="footer">
+            <div className="footer_left">
+                <img
+                    className="footer_albumLogo"
+                    src={
+                        "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/pride-album-cover-art-design-template-d2bf21df8ead7742d0af24f0143097df_screen.jpg?ts=1596467888"
+                    }
+                    alt="album_cover"
+                />
+                <div className="footer_songInfo">
+                    <h4>Starboy</h4>
+                    <p>The Weeknd</p>
+                </div>
+            </div>
+            <div className="footer_center">
+                <ShuffleIcon className="footer_green" />
+                <SkipPreviousIcon className="footer_icon" />
+                <PlayCircleOutlineIcon fontSize="large" className="footer_icon" />
+                <SkipNextIcon className="footer_icon" />
+                <RepeatIcon className="footer_green" />
+            </div>
+            <div className="footer_right">
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider />
+                    </Grid>
+                </Grid>
+            </div>
+        </div>
+    );
 }
 
 export default Footer;
