4c2e6d25890766207c38d7a42042f2efebd6378f
Jothinayagan <v.i.jothinayagan.307@gmail.com>
2022-03-09 17:49:48 +0530

    chore: Remove context API

--- src/Context/index.js
+++
@@ -1,9 +0,0 @@
-import React, { createContext, useReducer, useContext } from "react";
-
-export const SpotifyContext = createContext();
-
-export const SpotifyProvider = ({ initialState, reducer, children }) => (
-    <SpotifyContext.Provider value={useReducer(reducer, initialState)}>{children}</SpotifyContext.Provider>
-);
-
-export const useSpotify = () => useContext(SpotifyContext);
